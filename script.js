c833e564364c23b8fc081ae68beb9d7166877707
Abdul Kareem <mabkareem@gmail.com>
2024-03-17 16:29:06 -0400

    Add a basic 16x16 board

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ let body = document.querySelector("body");
 let containerDiv = document.createElement("div");
 containerDiv.classList.add("board");
 let blockDivs = [];
-for(let i = 0; i < 16; i++) {
-    blockDivs[i] = document.createElement("div");
-    blockDivs[i].addEventListener("mouseover", () => {
-        blockDivs[i].style.backgroundColor = "red";
-    })
-    blockDivs[i].classList.add("cell");
-    containerDiv.appendChild(blockDivs[i]);
-}
+let rowDivs = [];
 
+let i = 0;
+for (let k = 0; k < 16; k++) {
+    rowDivs[k] = document.createElement("div");
+    rowDivs[k].classList.add("row")
+    for(let j = 0; j < 16; j++) {
+        blockDivs[i] = document.createElement("div");
+        blockDivs[i].classList.add("cell");
+        console.log(i);
+        blockDivs[i].addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "red";
+        })
+        rowDivs[k].appendChild(blockDivs[i]);
+        i++
+    }
+    containerDiv.appendChild(rowDivs[k]);
+}
 body.appendChild(containerDiv);
 
