e8f401fa1cd73a21369e01494f380113d9f285cf
Abdul Kareem <mabkareem@gmail.com>
2024-03-17 17:46:07 -0400

    Refactor script.js and add CSS styling

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ let containerDiv = document.createElement("div");
 containerDiv.classList.add("board");
 let blockDivs = [];
 let rowDivs = [];
+createGrid(16);
 
-let i = 0;
-for (let k = 0; k < 16; k++) {
-    rowDivs[k] = document.createElement("div");
-    rowDivs[k].classList.add("row")
-    for(let j = 0; j < 16; j++) {
-        blockDivs[i] = document.createElement("div");
-        blockDivs[i].classList.add("cell");
-        console.log(i);
-        blockDivs[i].addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor = "red";
-        })
-        rowDivs[k].appendChild(blockDivs[i]);
-        i++
+
+
+
+
+function createGrid(size){
+    let i = 0;
+    for (let k = 0; k < size; k++) {
+        rowDivs[k] = document.createElement("div");
+        rowDivs[k].classList.add("row")
+        for(let j = 0; j < size; j++) {
+            blockDivs[i] = document.createElement("div");
+            blockDivs[i].classList.add("cell");
+            console.log(i);
+            blockDivs[i].addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = "#33186B";
+            })
+            rowDivs[k].appendChild(blockDivs[i]);
+            i++
+        }
+        containerDiv.appendChild(rowDivs[k]);
     }
-    containerDiv.appendChild(rowDivs[k]);
+    body.appendChild(containerDiv);
 }
-body.appendChild(containerDiv);
 
